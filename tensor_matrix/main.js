656a4a018ac6fccb9469f26063d03d7a6dd05a30
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-04-08 15:32:17 +0300

    fix format

--- tensor_matrix/main.js
+++ tensor_matrix/main.js
@@ -62,15 +62,15 @@ function computeElement(a, b) {
   let count = 0;
   
   for (let i = 0; i < arr.length; i++) {
-		for (let j = 0; j < arr.length; j++) {
-			if (arr[j] > arr[j + 1]) {
-				let tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+	      let tmp = arr[j];
+	      arr[j] = arr[j + 1];
+	      arr[j + 1] = tmp;
         count += 1;
-			}
-		}
-	}
+	    }
+	  }
+  }
   
   for (let i = 0; i < arr.length-1; i++) {
     if (arr[i] === arr[i+1]) {
