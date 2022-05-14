2f63f5d20d9d62244133e3e7e518257e7e2aa4c8
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2022-05-14 11:18:39 +0300

    update sphere

--- sphere/main.js
+++ sphere/main.js
@@ -147,3 +147,53 @@ void main() {
     frag_color = vec4(0.8, 0.5, 0.7, 1);
 }
 `;
+
+function buildProgram(vsSource, fsSource, attributes) {
+    const vs = compileShader(vsSource, 'vertex');
+    if (vs === null) { return null; }
+
+    const fs = compileShader(fsSource, 'fragment');
+    if (fs === null) { return null; }
+
+    const program = GL.createProgram();
+
+    for (const name in attributes) {
+        const index = attributes[name];
+
+        GL.bindAttribLocation(program, index, name);
+    }
+    
+    GL.attachShader(program, vs);
+    GL.attachShader(program, fs);
+    GL.linkProgram(program);
+
+    if (!GL.getProgramParameter(program, GL.LINK_STATUS)) { 
+        console.error(GL.getProgramInfoLog(program));
+
+        return null;
+    }
+
+    return program;
+}
+
+function compileShader(source, type) {
+    let glType = type;
+
+    if (type === 'vertex') { glType = GL.VERTEX_SHADER; }
+    else if (type === 'fragment') { glType = GL.FRAGMENT_SHADER; }
+
+    const shader = GL.createShader(glType);
+
+    GL.shaderSource(shader, source);
+    GL.compileShader(shader);
+
+
+    if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) { 
+        console.error(`SHADER TYPE ${type}`);
+        console.error(GL.getShaderInfoLog(shader));
+
+        return null;
+    }
+
+    return shader;
+}
